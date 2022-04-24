3ef239bb3bd305866de4b48e15fbf650dce166c9
mohammed-eddaghal <mohammed_eddaghal@um5.ac.ma>
2022-04-24 13:54:35 +0000

    i18n

--- src/main/java/com/mohammed/rest/webservices/controllers/UserController.java
+++ src/main/java/com/mohammed/rest/webservices/controllers/UserController.java
@@ -5,6 +5,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import com.mohammed.rest.webservices.entites.User;
 import com.mohammed.rest.webservices.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
@@ -13,24 +15,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
 
 @Controller
 @RequestMapping("api/v1")
 public class UserController {
     @Autowired
-    UserService userService;
+    private UserService userService;
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/users")
-    public ResponseEntity<List<EntityModel<User>>> getUsers(){
+    public ResponseEntity<List<EntityModel<User>>> getUsers() {
+
         List<User> userList = userService.findAllUsers();
 
         List<EntityModel<User>> entityModelList;
         entityModelList = userList.stream().map(user -> {
             EntityModel<User> model = EntityModel.of(user);
             WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).getOneUser(user.getId()));
-            model.add(linkBuilder.withRel("user "+user.getId()));
+            model.add(linkBuilder.withRel("user " + user.getId()));
             return model;
         }).toList();
 
@@ -38,7 +44,8 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<EntityModel<User>> getOneUser(@PathVariable Long id){
+    public ResponseEntity<EntityModel<User>> getOneUser(
+            @PathVariable Long id) {
         User user = userService.findOneUser(id);
         EntityModel<User> model = EntityModel.of(user);
         WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).getUsers());
@@ -49,7 +56,27 @@ public class UserController {
     }
 
     @PostMapping("/users/add")
-    public ResponseEntity<User> addUser (@Valid @RequestBody User user){
+    public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/helloworld_i18n", headers="Accept=application/json")
+    public String helloworld(
+            //@RequestHeader(name = "Accept-Language", defaultValue = "en", required = false) Locale locale
+    ) {
+
+        System.out.println(messageSource.getMessage(
+                "language",
+                null,
+                "default message",
+                LocaleContextHolder.getLocale()));
+
+
+        String message = messageSource.getMessage(
+                "language",
+                null,
+                "default message",
+                LocaleContextHolder.getLocale());
+        return message;
     }
 }
